fc7867eb082c907562ad436290fbeeefe52fd63f
cwonrails <cwonrails@gmail.com>
2015-06-02 09:16:08 -0500

    A few updates to arch scripts.

--- arch/archscripts/official.sh
+++ arch/archscripts/official.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sudo pacman -Syu --needed --noconfirm base base-devel grub sudo git python2 i3 dmenu vim tmux npm nodejs subversion mercurial chromium firefox wget openssh rsync htop lynx xf86-input-synaptics nginx redis virtualbox os-prober docker ansible openbox bzr vagrant expac yajl bash-completion xf86-video-intel xorg-server xorg-server-utils xorg-xinit tarsnap openssh hub mesa xfce4 openssl docker xfce4-goodies s3cmd phantomjs libyaml postgresql postgresql-libs ctags efibootmgr
+sudo pacman -Syu --needed --noconfirm base base-devel grub sudo git python2 i3 dmenu vim tmux npm nodejs subversion mercurial chromium firefox wget openssh rsync htop lynx xf86-input-synaptics nginx redis os-prober docker ansible bzr vagrant expac yajl bash-completion xf86-video-intel xorg-server xorg-server-utils xorg-xinit tarsnap hub openssl docker s3cmd phantomjs libyaml postgresql postgresql-libs efibootmgr lxc puppet iw wpa_supplicant

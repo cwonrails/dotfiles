bbd443cbc1b0a657ca9070ca676d318a6c02c3c7
Chris Watson <cwonrails@gmail.com>
2016-12-29 10:56:41 -0500

    renamed ubuntu folder to trusty, changed nginx sources list

--- trusty/setup.sh
+++ trusty/setup.sh
@@ -51,8 +51,8 @@ mkdir ~/gitlab
 
 # nginx
 wget http://nginx.org/keys/nginx_signing.key | sudo apt-key add -
-echo "deb http://nginx.org/packages/ubuntu/ xenial nginx" | sudo tee -a /etc/apt/sources.list
-echo "deb-src http://nginx.org/packages/ubuntu/ xenial nginx" | sudo tee -a /etc/apt/sources.list
+echo "deb http://nginx.org/packages/ubuntu/ trusty nginx" | sudo tee -a /etc/apt/sources.list
+echo "deb-src http://nginx.org/packages/ubuntu/ trusty nginx" | sudo tee -a /etc/apt/sources.list
 sudo apt-get update
 sudo apt-get -y install nginx
 
